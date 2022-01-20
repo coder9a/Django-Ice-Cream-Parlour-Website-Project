60f3bc6175c2239cd59b84a2d2fcfbd3a1fcc48d
Raj Balhara <42884781+coder9a@users.noreply.github.com>
2022-01-20 16:02:44 +0530

    Update settings.py

--- hello/settings.py
+++ hello/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(39pekmkoht%a1r$5a19osyb*!nfe0ve417k6y1+kkz#pqo+ri'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
